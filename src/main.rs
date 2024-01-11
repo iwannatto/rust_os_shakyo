62eeae707b890e134ffb51a5b50ef4cb9142390d
iwannatto <iwannatto@gmail.com>
2024-01-11 11:10:32 +0900

    VGA text mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+use crate::vga_buffer::WRITER;
+use core::fmt::Write;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
+
+    loop {}
 }
